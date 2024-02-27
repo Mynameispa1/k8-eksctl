bf363684f90a95d6133b94cc8ef86ae072cc353d
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2024-02-26 21:48:58 -0600

    k8-eksctl

---
+++ workstation.tf
@@ -0,0 +1,62 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "workstation-eksctl"
+  ami = data.aws_ami.centos.id
+  instance_type          = "t2.micro"
+#   key_name               = "user1"
+#   monitoring             = true
+  vpc_security_group_ids = [aws_security_group.allow_eksctl.id]
+  subnet_id              = "subnet-0a0d059e29a18bbce" #replace your default subnet id
+  user_data = file ("workstation.sh")
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_security_group" "allow_eksctl" {
+  name        = "allow_eksctl"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+#   vpc_id      = aws_vpc.main.id
+ ingress {
+    description = "all ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_eksctl"
+  }
+}
+
+
+data "aws_ami" "centos" {
+    owners = ["973714476881"]
+    most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Centos-8-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_vpc" "default" {
+    default = true
+
+}
